f6f2e6b0352a1b73d840dd3798f7e13f5a99cb51
Sangguk Lee <sangguk258@gmail.com>
2017-04-07 19:57:42 +0900

    Init Project

---
+++ Assets/drum.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+//Add this Script Directly to The Death Zone
+
+public class drum : MonoBehaviour {
+    void Start()
+    {
+        //GetComponent<AudioSource>().playOnAwake = false;
+        //GetComponent<AudioSource>().clip = saw;
+    }
+
+    void OnTriggerEnter(Collider col)  //Plays Sound Whenever collision detected
+    {
+        GetComponent<AudioSource>().Play();
+    }
+    // Make sure that deathzone has a collider, box, or mesh.. ect..,
+    // Make sure to turn "off" collider trigger for your deathzone Area;
+    // Make sure That anything that collides into deathzone, is rigidbody;
+}
