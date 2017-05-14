85dd3d440c927288a5f3eca34f72c4794d191bac
Luan Sangguk Lee <sangguk258@gmail.com>
2017-05-15 02:22:23 +0900

    draw line 구현

--- Assets/Bolbbalgan Drum/Scripts/classes/StickRecognizer.cs
+++ Assets/Bolbbalgan Drum/Scripts/classes/StickRecognizer.cs
@@ -101,12 +101,53 @@ public class StickRecognizer {
                 colorData[3*i] = (byte) (255*(manager.DepthData[i] / depthLimt));
         }
 
-        colorData[3 * leftIdx + 1] = 255;
-        colorData[3 * rightIdx + 1] = 255;
-
         debugTexture.LoadRawTextureData(colorData);
+        DrawLine(debugTexture, leftIdx%manager.DepthFrameDesc.Width, (int) leftIdx/manager.DepthFrameDesc.Width,
+                                rightIdx%manager.DepthFrameDesc.Width, (int) rightIdx/manager.DepthFrameDesc.Width, Golor.green);
         debugTexture.Apply();
         debugPlane.GetComponent<Renderer>().material.mainTexture = debugTexture;
+    }
+
+    void DrawLine(Texture2D tex, int x1, int y1, int x2, int y2, Color col)
+    {
+        int dy = (int)(y1-y0);
+        int dx = (int)(x1-x0);
+        int stepx, stepy;
+     
+        if (dy < 0) {dy = -dy; stepy = -1;}
+        else {stepy = 1;}
+        if (dx < 0) {dx = -dx; stepx = -1;}
+        else {stepx = 1;}
+        dy <<= 1;
+        dx <<= 1;
+     
+        float fraction = 0;
+     
+        tex.SetPixel(x0, y0, col);
+        if (dx > dy) {
+            fraction = dy - (dx >> 1);
+            while (Mathf.Abs(x0 - x1) > 1) {
+                if (fraction >= 0) {
+                    y0 += stepy;
+                    fraction -= dx;
+                }
+                x0 += stepx;
+                fraction += dy;
+                tex.SetPixel(x0, y0, col);
+            }
+        }
+        else {
+            fraction = dx - (dy >> 1);
+            while (Mathf.Abs(y0 - y1) > 1) {
+                if (fraction >= 0) {
+                    x0 += stepx;
+                    fraction -= dy;
+                }
+                y0 += stepy;
+                fraction += dx;
+                tex.SetPixel(x0, y0, col);
+            }
+        }
     }
 
     private int GetStickEnd(ushort[] depthData, Kinect.CameraSpacePoint handCameraPoint, Kinect.DepthSpacePoint handDepthPoint)
